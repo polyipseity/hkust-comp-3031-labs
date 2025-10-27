694a5437e90e77e5fb94c320252db359e211b6ae
William So <polyipseity@gmail.com>
2025-10-27 18:36:22 +0800

    Complete lab 4

--- lab-4/src/main/scala/lab4/Anagrams.scala
+++ lab-4/src/main/scala/lab4/Anagrams.scala
@@ -1,5 +1,6 @@
 package lab4
 
+import scala.annotation.tailrec
 import scala.io.{Codec, Source}
 
 object Anagrams extends AnagramsInterface:
@@ -35,10 +36,12 @@ object Anagrams extends AnagramsInterface:
    *
    * Note: you must use `groupBy` to implement this method!
    */
-  def wordOccurrences(w: Word): Occurrences = ???
+  def wordOccurrences(w: Word): Occurrences =
+    w.groupBy(_.toLower).view.mapValues(_.length).toList.sortBy(_._1)
 
   /** Converts a sentence into its character occurrence list. */
-  def sentenceOccurrences(s: Sentence): Occurrences = ???
+  def sentenceOccurrences(s: Sentence): Occurrences =
+    s.flatMap(wordOccurrences).groupMapReduce(_._1)(_._2)(_ + _).toList.sortBy(_._1)
 
   /** The `dictionaryByOccurrences` is a `Map` from different occurrences to a sequence of all
    * the words that have that occurrence count.
@@ -55,10 +58,12 @@ object Anagrams extends AnagramsInterface:
    * List(('a', 1), ('e', 1), ('t', 1)) -> Seq("ate", "eat", "tea")
    *
    */
-  lazy val dictionaryByOccurrences: Map[Occurrences, List[Word]] = ???
+  lazy val dictionaryByOccurrences: Map[Occurrences, List[Word]] =
+    dictionary.groupBy(wordOccurrences).view.mapValues(_.sorted).toMap.withDefaultValue(Nil)
 
   /** Returns all the anagrams of a given word. */
-  def wordAnagrams(word: Word): List[Word] = ???
+  def wordAnagrams(word: Word): List[Word] =
+    dictionaryByOccurrences(wordOccurrences(word))
 
   /** Returns the list of all subsets of the occurrence list.
    * This includes the occurrence itself, i.e. `List(('k', 1), ('o', 1))`
@@ -82,7 +87,19 @@ object Anagrams extends AnagramsInterface:
    * Note that the order of the occurrence list subsets does not matter -- the subsets
    * in the example above could have been displayed in some other order.
    */
-  def combinations(occurrences: Occurrences): List[Occurrences] = ???
+  def combinations(occurrences: Occurrences): List[Occurrences] = {
+    @tailrec
+    def impl(ret: List[Occurrences], occurrences: Occurrences): List[Occurrences] =
+      occurrences match {
+        case (char, count) :: next => impl(
+          ret.flatMap(occurrence => occurrence :: (1 to count).map((char, _) :: occurrence).toList),
+          next,
+        )
+        case Nil => ret
+      }
+
+    impl(List(Nil), occurrences.reverse)
+  }
 
   /** Subtracts occurrence list `y` from occurrence list `x`.
    *
@@ -94,7 +111,13 @@ object Anagrams extends AnagramsInterface:
    * Note: the resulting value is an occurrence - meaning it is sorted
    * and has no zero-entries.
    */
-  def subtract(x: Occurrences, y: Occurrences): Occurrences = ???
+  def subtract(x: Occurrences, y: Occurrences): Occurrences =
+    (x ::: y.map((char, count) => (char, -count)))
+      .groupMapReduce(_._1)(_._2)(_ + _)
+      .tapEach((char, count) => if count < 0 then throw IllegalArgumentException((char, count).toString) else ())
+      .filter(_._2 != 0)
+      .toList
+      .sortBy(_._1)
 
   /** Returns a list of all anagram sentences of the given sentence.
    *
@@ -136,7 +159,22 @@ object Anagrams extends AnagramsInterface:
    *
    * Note: There is only one anagram of an empty sentence.
    */
-  def sentenceAnagrams(sentence: Sentence): List[Sentence] = ???
+  def sentenceAnagrams(sentence: Sentence): List[Sentence] =
+    @tailrec
+    def impl(ret: List[(Occurrences, List[Sentence])]): List[Sentence] = {
+      if ret.forall(_._1.isEmpty) then ret.flatMap(_._2) else
+        impl(ret.flatMap((occurrences, sentences) => {
+          if occurrences.isEmpty then List((occurrences, sentences)) else
+            combinations(occurrences)
+              .flatMap(combination => {
+                val remaining = subtract(occurrences, combination)
+                dictionaryByOccurrences(combination)
+                  .map(word => (remaining, sentences.map(word :: _)))
+              })
+        }))
+    }
+
+    impl(List((sentenceOccurrences(sentence), List(Nil))))
 
 object Dictionary:
   def loadDictionary: List[String] =
